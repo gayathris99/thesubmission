99022abdfe21ab24a113fedd93ac92a122298a20
gayathris99 <gayathri11499@gmail.com>
2024-07-25 20:27:32 +0530

    Created Background and header for all Pages

--- src/pages/RulesPage.vue
+++ src/pages/RulesPage.vue
@@ -1,3 +1,36 @@
 <template>
-  <div>RULES PAGE</div>
+  <div class="bg-image color-white font-helsa">
+    <div class="header row items-center align-center justify-center q-gutter-x-xl q-pt-xl fw-900 fs-48">
+      <div class="cursor-pointer" @click="goTo('home')">HOME</div>
+      <div  class="cursor-pointer" @click="goTo('surrender')">SURRENDER</div>
+      <div  class="cursor-pointer" @click="goTo('contact')">CONTACT</div>
+    </div>
+    <div class="form">
+
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'RulesPage',
+  methods: {
+    goTo (name) {
+      this.$router.push({
+        name
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.bg-image {
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+  background-image: url('~assets/blackbackground.jpg');
+}
+</style>
+
--- src/pages/SurrenderPage.vue
+++ src/pages/SurrenderPage.vue
@@ -1,3 +1,35 @@
 <template>
-  <div>SURRENDER PAGE</div>
+  <div class="bg-image color-white font-helsa">
+    <div class="header row items-center align-center justify-center q-gutter-x-xl q-pt-xl fw-900 fs-48">
+      <div class="cursor-pointer" @click="goTo('home')">HOME</div>
+      <div  class="cursor-pointer" @click="goTo('rules')">RULES</div>
+      <div  class="cursor-pointer" @click="goTo('contact')">CONTACT</div>
+    </div>
+    <div class="form">
+      
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'SurrenderPage',
+  methods: {
+    goTo (name) {
+      this.$router.push({
+        name
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.bg-image {
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+  background-image: url('~assets/blackbackground.jpg');
+}
+</style>
