2b6ee97c1c9bd071dd2f5294024f4eed5885b7a4
gayathris99 <gayathri11499@gmail.com>
2024-07-27 21:46:43 +0530

    Added notification and route to rules after surrender

--- src/pages/SurrenderPage.vue
+++ src/pages/SurrenderPage.vue
@@ -10,21 +10,37 @@
       <div class="fs-36 fw-600 text-center subtitle animate-flicker">
         A one way ticket to the Black Site. <br/>Fill the details, clench your thighs shut and wait for your instructions.
       </div>
-      <q-form class="form">
+      <q-form class="form" @submit.prevent.stop="onSubmit">
         <div class="column align-center  q-gutter-x-xs q-mt-xl color-white fw-600 fs-36 subtitle ">
           <div class="q-mb-sm">1. What days of the week are you usually on SL ?</div>
-          <q-input v-model="daysOfWeek" dark outlined  class="full-width fs-36 subtitle"></q-input>
+          <q-input
+            v-model="daysOfWeek"
+            dark
+            outlined
+            class="full-width fs-36 subtitle font-opti"
+            :rules="[val => !!val || 'Field is required']"/>
         </div>
         <div class="column align-center  q-gutter-x-xs q-mt-xl color-white fw-600 fs-36 subtitle ">
-          <div class="q-mb-sm">2. What days of the week are you usually on SL ?</div>
-          <q-input v-model="daysOfWeek" dark outlined  class="full-width fs-36 subtitle"></q-input>
+          <div class="q-mb-sm">2. What is your regular online hours in SL?</div>
+          <q-input
+          v-model="onlineHours"
+          dark
+          outlined
+          class="full-width fs-36 subtitle font-opti"
+          :rules="[val => !!val || 'Field is required',
+          val => val.match(/^[0-9]+$/) || 'Only numbers allowed']"/>
         </div>
         <div class="column align-center  q-gutter-x-xs q-mt-xl color-white fw-600 fs-36 subtitle ">
-          <div class="q-mb-sm">3. What days of the week are you usually on SL ?</div>
-          <q-input v-model="daysOfWeek" dark outlined  class="full-width fs-36 subtitle"></q-input>
+          <div class="q-mb-sm">3. Please inform the one limit you are permitted below, that is on top of the default prison limits.</div>
+          <q-input
+          v-model="limitPermitted"
+          dark
+          outlined
+          class="full-width fs-36 subtitle font-opti"
+          :rules="[val => !!val || 'Field is required']"/>
         </div>
         <div class="row items-center q-my-xl">
-          <q-btn label="Surrender" color="black" dense push class="full-width glossy subtitle fs-32 fw-700" @click="onSubmit"/>
+          <q-btn label="Surrender" type="submit" color="black" dense push class="full-width glossy subtitle fs-32 fw-700" :disable="!isAllValid"/>
         </div>
       </q-form>
     </div>
@@ -38,7 +54,14 @@ export default {
   name: 'SurrenderPage',
   data () {
     return {
-      daysOfWeek: ''
+      daysOfWeek: '',
+      onlineHours: '',
+      limitPermitted: ''
+    }
+  },
+  computed: {
+    isAllValid () {
+      return this.daysOfWeek && this.onlineHours && this.limitPermitted
     }
   },
   methods: {
@@ -49,20 +72,32 @@ export default {
     },
     onSubmit (e) {
       e.preventDefault();
-     axios.defaults.headers.post["Content-Type"] = "application/json";
-      Loading.show()
-      axios
-        .post("https://formsubmit.co/ajax/misamai921@gmail.com", {
-          DaysOfWeekOnSL: this.daysOfWeek
-        })
-        .then((response) => {
-          console.log(response)
-          Loading.hide()
-        })
-        .catch((error) => {
-          console.log(error)
-          Loading.hide()
-        })
+      if (this.isAllValid) {
+        axios.defaults.headers.post["Content-Type"] = "application/json";
+          Loading.show()
+          axios
+            .post("https://formsubmit.co/ajax/misamai921@gmail.com", {
+              DaysOfWeekOnSL: this.daysOfWeek,
+              RegularOnlineHours: this.onlineHours,
+              OneLimitPermitted: this.limitPermitted
+            })
+            .then((response) => {
+              console.log(response)
+              Loading.hide()
+              this.$q.notify({
+                color: 'red',
+                message: 'You are one step closer to Hell.',
+                caption: 'You\'ll hear from us soon.',
+                position: "top",
+                classes: 'font-opti fs-32 fw-700 text-center'
+              })
+              this.$router.push({ name: 'rules' })
+            })
+            .catch((error) => {
+              console.log(error)
+              Loading.hide()
+            })
+      }
     }
   }
 }
@@ -79,14 +114,13 @@ export default {
   width: 100%;
   height: 100%;
 }
-.q-card {
-  width: 100%;
-  max-width: 65vw;
-}
 .subtitle {
   letter-spacing: 0.075rem;
 }
 .form {
   width: 65vw
+}
+:deep(.q-field__messages) {
+  font-size: 18px;
 }
 </style>
