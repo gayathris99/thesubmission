d39087ec1b9ed88d802040e25b207b13913df78d
gayathris99 <gayathri11499@gmail.com>
2024-07-27 19:14:40 +0530

    Gallery Images

--- src/pages/GalleryPage.vue
+++ src/pages/GalleryPage.vue
@@ -6,6 +6,56 @@
       <div  class="cursor-pointer" @click="goTo('rules')">RULES</div>
       <div  class="cursor-pointer" @click="goTo('contact')">CONTACT</div>
     </div>
+    <div class="gallery-container row items-center align-center justify-evenly q-pb-xl q-px-md">
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image1.png" alt="">
+      </div>
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image2.png" alt="">
+      </div>
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image3.png" alt="">
+      </div>
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image4.png" alt="">
+      </div>
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image5.png" alt="">
+      </div>
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image6.png" alt="">
+      </div>
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image7.png" alt="">
+      </div>
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image8.png" alt="">
+      </div>
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image9.png" alt="">
+      </div>
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image10.png" alt="">
+      </div>
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image11.png" alt="">
+      </div>
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image12.png" alt="">
+      </div>
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image13.png" alt="">
+      </div>
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image14.png" alt="">
+      </div>
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image15.png" alt="">
+      </div>
+      <div class="col-4 q-mt-xl">
+        <img src="~assets/TSMGallery/image16.png" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,8 +67,11 @@ export default {
       this.$router.push({
         name
       })
-    }
+    },
+  returnImage (index) {
+    return `~assets/TSMGallery/image${index+1}.png`
   }
+  },
 }
 </script>
 
@@ -27,8 +80,13 @@ export default {
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-  height: 100vh;
   background-image: url('~assets/blackbackground.jpg');
+}
+.gallery-container {
+  img {
+    width: 450px;
+    height: 300px;
+  }
 }
 </style>
 
