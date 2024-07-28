523a7394c954cde82c81e83f85a464cf1c811e75
gayathris99 <gayathri11499@gmail.com>
2024-07-28 11:00:48 +0530

    Contact Page

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -6,6 +6,16 @@
       <div  class="cursor-pointer" @click="goTo('rules')">RULES</div>
       <div  class="cursor-pointer" @click="goTo('gallery')">GALLERY</div>
     </div>
+    <div class="contact-container q-mt-xl q-px-xl">
+      <div class="row items-center align-center q-gutter-md justify-center">
+        <img src="~assets/TSMContacts/Wyattt.jpg" alt="">
+        <div class="column q-pl-xl">
+          <div class="fs-48 fw-600">Wyatt Brennan</div>
+          <div class="fs-32 fw-600">Owner of The Sub Mission</div>
+          <div class="fs-32 fw-600">For any further information, reach out to the Warden<span class="font-opti"> (wyattbrennan)</span></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -29,6 +39,12 @@ export default {
   background-size: cover;
   height: 100vh;
   background-image: url('~assets/blackbackground.jpg');
+}
+.contact-container {
+  img {
+    width: 450px;
+    height: 340px;
+  }
 }
 </style>
 
